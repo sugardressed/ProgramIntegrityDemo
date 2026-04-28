b332e86af7e4e29fc63412c5a2942db64e346af0
erivas <sugardressed@gmail.com>
2026-04-28 05:59:14 -0700

    Demo Track System

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+//output error message
+    setcookie('login_okay','no');
+    echo 'You are successfully log out. To log in again please click <a href="index.html">Here</a>';
+    exit();
+
+?>
